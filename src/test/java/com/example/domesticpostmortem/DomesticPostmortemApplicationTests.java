dfff276044f7015f6110397665f9fad59924f0c2
y-susaki <y_susaki@partner.h-t.co.jp>
2026-06-20 19:37:58 +0900

    first push

---
+++ src/test/java/com/example/domesticpostmortem/DomesticPostmortemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.domesticpostmortem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomesticPostmortemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
